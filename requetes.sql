b7473281b8b76f1e4852d00772e5d40a37e12bd0
PeRiOuS-AD <aminedridi181@gmail.com>
2025-12-05 11:42:55 +0100

    requests.sql est la page ou nos requetes et reponses aux questions demandées pourle moment tous les question commentés mais rien n'est fait encore là dessus

---
+++ requetes.sql
@@ -0,0 +1,25 @@
+-- --1. Le nom et le grade des encadrants du doctorant dont l’identifiant est "d001".
+-- --2. Le nom et le pays des auteurs collaborateurs (auteurs externes) du chercheur "Jean Azi" de 2016 à 2020.
+-- --3. Le nombre de collaborateurs total du chercheur d’identifiant "S001".
+-- --4. Le nombre de pays avec lesquels le LAAS a collaboré dans le cadre de publications de rang A (i.e. le nombre d’articles publiés dans des conférences de classe A).
+-- --5. Pour les doctorants, on souhaiterait récupérer le nombre de leurs publications. On veut retourner l’identifiant de chaque doctorant accompagné du nombre de ses publications.
+-- --6. Le nombre de doctorants du laboratoire ayant soutenu
+
+-- 7. Le nom et le prénom des chercheurs qui n’ont jamais encadré.
+-- 8. Le nom et le prénom des chercheurs qui n’ont jamais publié, ni encadré.
+-- 9. Le nom et prénom des chercheurs qui encadrent mais n’ont pas de doctorants ayant déjà
+-- soutenu.
+-- 10. L’identifiant, nom et prénom des doctorants qui ont un seul encadrant.
+-- 11. Les chercheurs qui ont plus de 4 doctorants en cours. Pour chaque chercheur, on veut afficher
+-- son identifiant, son nom, son prenom, et son nombre de doctorants.
+-- 12. L’identifiant des chercheurs qui n’ont publié que dans des conférences de classe A.
+-- 13. Nom, prénom et identifiant des chercheurs qui ont encadré tous les doctorants.
+-- 14. Le nombre de publications du laboratoire par année.
+-- 15. Le nombre d’enseignants chercheurs par établissement d’enseignement.
+-- 16. Le(s) pays avec lequel le laboratoire a le plus de publications.
+-- 17. Les scientifiques qui ont un seul projet.
+-- 18. Les scientifiques qui ont participé à tous les projets.
+-- 19. Les établissements d’enseignements ayant plus de 50 enseignants-chercheurs appartenant au
+-- LAAS.
+-- 20. Les scientifiques qui ont le plus de projets.
+-- 21. Les pays qui sont impliqués dans tous les projets.
