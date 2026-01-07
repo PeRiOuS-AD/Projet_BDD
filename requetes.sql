838406ad0836ae023ac2e58542aafeaf170d4d30
PeRiOuS-AD <aminedridi181@gmail.com>
2026-01-07 14:46:08 +0100

    from 11 to 16 the questions are done

--- requetes.sql
+++ requetes.sql
@@ -1,4 +1,6 @@
--- --1. Le nom et le grade des encadrants du doctorant dont l’identifiant est "d001".
+DROP VIEW publication_pas_que_A;
+DROP VIEW publication_par_pays;
+ --1. Le nom et le grade des encadrants du doctorant dont l’identifiant est "d001".
 SELECT nom, grade 
 FROM scientifique 
 JOIN personnel ON id_personnel = id_scientifique 
@@ -88,11 +90,73 @@ WHERE 1 = (
 );
 -- 11. Les chercheurs qui ont plus de 4 doctorants en cours. Pour chaque chercheur, on veut afficher
 -- son identifiant, son nom, son prenom, et son nombre de doctorants.
+"""
+SELECT p.id_personnel, p.nom, p.prenom, COUNT(e.id_doc) AS nb_doctorants
+FROM Personnel p
+INNER JOIN Scientifique s ON p.id_personnel = s.id_scientifique
+INNER JOIN Chercheur c ON s.id_scientifique = c.id_chercheur
+INNER JOIN Encadrer e ON c.id_chercheur = e.id_scientifique
+INNER JOIN Doctorant d ON e.id_doc = d.id_doc
+--WHERE d.date_soutenance IS NULL OR d.date_soutenance > CURRENT_DATE
+GROUP BY p.id_personnel, p.nom, p.prenom
+HAVING COUNT(e.id_doc) > 4;
+"""
 -- 12. L’identifiant des chercheurs qui n’ont publié que dans des conférences de classe A.
+'''
+CREATE VIEW publication_pas_que_A AS
+SELECT ppp.id_personnel
+FROM ParticipationPersonelPubli ppp
+INNER JOIN Publication p ON ppp.id_publication = p.id_publication
+WHERE p.classe <> 'A';
+
+SELECT DISTINCT pp.id_personnel
+FROM ParticipationPersonelPubli pp
+INNER JOIN Publication pub ON pp.id_publication = pub.id_publication
+INNER JOIN Scientifique s ON pp.id_personnel = s.id_scientifique
+WHERE pub.classe = 'A' AND pp.id_personnel NOT IN (SELECT id_personnel FROM publication_pas_que_A);
+'''
 -- 13. Nom, prénom et identifiant des chercheurs qui ont encadré tous les doctorants.
+"""
+SELECT 
+    S.id_scientifique, 
+    P.nom, 
+    P.prenom
+FROM Scientifique S
+INNER JOIN Personnel P ON S.id_scientifique = P.id_personnel
+INNER JOIN Encadrer E ON S.id_scientifique = E.id_scientifique
+GROUP BY S.id_scientifique, P.nom, P.prenom
+HAVING COUNT(DISTINCT E.id_doc) = (
+    SELECT COUNT(*) FROM Doctorant   -- Nombre total de doctorants dans la base
+);
+"""
 -- 14. Le nombre de publications du laboratoire par année.
+"""
+SELECT annee, COUNT(id_publication) AS nb_pub
+FROM Publication
+GROUP BY annee
+ORDER BY annee ;
+"""
 -- 15. Le nombre d’enseignants chercheurs par établissement d’enseignement.
--- 16. Le(s) pays avec lequel le laboratoire a le plus de publications.
+"""
+SELECT et.nom, COUNT(ec.id_ec) AS nb_enseignants_chercheurs
+FROM Etablissement et
+INNER JOIN Enseignant_chercheur ec ON et.id_etablissement = ec.id_etablissement
+GROUP BY et.id_etablissement, et.nom;
+"""
+--Q16:Les pays avec lequel le laboratoire a le plus de publications
+"""
+CREATE VIEW publication_par_pays AS
+SELECT l.pays,COUNT(p.id_publication) AS nb_pubs
+FROM Publication p
+INNER JOIN ParticipationAEPubli pae ON p.id_publication = pae.id_publication
+INNER JOIN Auteur_externe ae ON pae.id_a_externe = ae.id_a_externe
+INNER JOIN Lab_externe l ON ae.id_l_externe = l.id_l_externe
+GROUP BY l.pays;
+
+SELECT pays, nb_pubs
+FROM publication_par_pays
+WHERE nb_pubs >= (SELECT MAX(nb_pubs) FROM publication_par_pays);
+"""
 -- 17. Les scientifiques qui ont un seul projet.
 -- 18. Les scientifiques qui ont participé à tous les projets.
 -- 19. Les établissements d’enseignements ayant plus de 50 enseignants-chercheurs appartenant au
